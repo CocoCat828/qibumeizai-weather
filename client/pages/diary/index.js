15fd065a73c3ea0c5a834073b7cd438497ac5e87
SuGuansheng <513933967@qq.com>
2020-05-29 10:14:52 +0800

    新增心情签到页面

---
+++ client/pages/diary/index.js
@@ -0,0 +1,36 @@
+Page({
+  data: {
+    emtions: ['serene', 'hehe', 'ecstatic', 'sad', 'terrified'],
+    colors: {
+      serene: '#64d9fe',
+      hehe: '#d3fc1e',
+      ecstatic: '#f7dc0e',
+      sad: '#ec238a',
+      terrified: '#ee1aea'
+    }
+  },
+  onLoad: function(options) {
+    //Do some initialize when page load.
+
+  },
+  onReady: function() {
+    //Do some when page ready.
+
+  },
+  onShow: function() {
+    //Do some when page show.
+
+  },
+  onHide: function() {
+    //Do some when page hide.
+
+  },
+  onUnload: function() {
+    //Do some when page unload.
+
+  },
+  onPullDownRefresh: function() {
+    //Do some when page pull down.
+
+  }
+})
