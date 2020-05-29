ac5101c734cd750e3502f1ee94fbac680e96cfbd
SuGuansheng <513933967@qq.com>
2020-05-29 11:59:40 +0800

    完善心情签到页面的页面结构和样式

--- client/pages/diary/index.js
+++ client/pages/diary/index.js
@@ -9,28 +9,7 @@ Page({
       terrified: '#ee1aea'
     }
   },
-  onLoad: function(options) {
-    //Do some initialize when page load.
-
+  goBack() {
+    wx.wx.navigateBack();
   },
-  onReady: function() {
-    //Do some when page ready.
-
-  },
-  onShow: function() {
-    //Do some when page show.
-
-  },
-  onHide: function() {
-    //Do some when page hide.
-
-  },
-  onUnload: function() {
-    //Do some when page unload.
-
-  },
-  onPullDownRefresh: function() {
-    //Do some when page pull down.
-
-  }
 })
